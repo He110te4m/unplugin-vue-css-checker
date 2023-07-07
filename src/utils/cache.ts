51f62a8c186f0e449ea599525f39d721e8fe108a
He110te4m <me@he110.site>
2023-07-07 11:51:52 +0800

    feat: add cache expired policy

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,10 +1,16 @@
 import { existsSync, readFileSync, readdirSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { ensurePath } from './file'
+import { getFileHash } from './hash'
+
+interface CacheItem {
+  hash: string
+  selectors: string[]
+}
 
 /** 选择器缓存类 */
 export class SelectorCache {
-  private cache: Map<string, string[]>
+  private cache: Map<string, CacheItem>
   private cacheDir: string
 
   constructor(cacheDir: string) {
@@ -12,47 +18,53 @@ export class SelectorCache {
     this.cache = new Map()
   }
 
-  get(file: string): string[] | undefined
-  get(file: string, defaultValue: string[]): string[]
-  get(file: string, defaultValue?: string[] | undefined): string[] | string[] | undefined {
-    return this.cache.get(file) ?? defaultValue
+  get(file: string): CacheItem['selectors'] | undefined
+  get(file: string, defaultValue: CacheItem['selectors']): CacheItem['selectors']
+  get(file: string, defaultValue?: CacheItem['selectors'] | undefined): CacheItem['selectors'] | undefined {
+    return this.cache.get(file)?.selectors ?? defaultValue
   }
 
-  set(file: string, data: string[]): void {
-    this.cache.set(file, data)
+  async set(file: string, selectors: string[]): Promise<void> {
+    const hash = await getFileHash(file)
+
+    this.cache.set(file, { hash, selectors })
   }
 
   all() {
-    return Array.from(new Set(Array.from(this.cache.values()).flat()))
+    const selectors = Array.from(this.cache.values())
+      .map(({ selectors }) => selectors)
+      .flat()
+
+    return Array.from(new Set(selectors))
   }
 
   /**
    * 加载缓存数据
    * TODO: 后续关注下性能，是否有必要换成异步的
    */
-  load() {
+  async load() {
     if (!existsSync(this.cacheDir))
       return
 
     const cacheKeys = Array.from(this.cache.keys())
 
-    readdirSync(this.cacheDir)
+    const list = readdirSync(this.cacheDir)
       .filter(filename => !cacheKeys.includes(this.filenameToKey(filename)))
-      .map(filename => ({
-        filename,
-        content: readFileSync(join(this.cacheDir, filename), 'utf8').toString(),
-      }))
-      .forEach(({ filename, content }) => {
-        let selectors: string[] = []
-
+      .map(async (filename) => {
+        const content = readFileSync(join(this.cacheDir, filename), 'utf8').toString()
+        let data: CacheItem = { hash: '', selectors: [] }
         try {
-          selectors = JSON.parse(content)
+          data = JSON.parse(content)
         }
         catch (error) {
         }
 
-        this.cache.set(filename, selectors)
+        const newHash = await getFileHash(filename)
+        if (newHash && newHash === data.hash)
+          this.cache.set(filename, data)
       })
+
+    await Promise.allSettled(list)
   }
 
   store() {
@@ -60,7 +72,7 @@ export class SelectorCache {
     Array.from(this.cache.keys())
       .forEach((key) => {
         const filename = this.keyToFilename(key)
-        const content = JSON.stringify(this.cache.get(key) ?? [])
+        const content = JSON.stringify(this.cache.get(key) ?? {})
 
         writeFileSync(join(this.cacheDir, filename), content)
       })
